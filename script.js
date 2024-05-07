bd048abca6a13725dda1270da8f394ad872a2a64
rafay183 <rafayshaikh633@gmail.com>
2024-05-07 10:34:06 +0500

    coming soon page

---
+++ script.js
@@ -0,0 +1,7 @@
+const subscribeBtn = document.getElementById('subscribe-btn');
+
+subscribeBtn.addEventListener('click', () => {
+  // Simulate form submission (replace with your actual logic)
+  alert('Thanks for subscribing! We will notify you soon.');
+
+});
