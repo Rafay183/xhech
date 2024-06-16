049351275362112493853c11f7820d0b54b2a0af
Muhammad Rafay Shaikh <50517621+Rafay183@users.noreply.github.com>
2024-06-16 23:33:58 +0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const subscribeBtn = document.getElementById('subscribe-btn');
-
-subscribeBtn.addEventListener('click', () => {
-  // Simulate form submission (replace with your actual logic)
-  alert('Thanks for subscribing! We will notify you soon.');
-
-});
